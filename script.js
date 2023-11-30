b755a297e22705569e3f4b166d8ed5b3b8e11893
EugeneDubrova777 <blitzkriegbop777@gmail.com>
2023-11-30 12:45:25 +0300

    feat: add test unfinished developments

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const kick = document.querySelector('.kick');
+const audioKick = document.querySelector('.a-kick');
+
+const crash = document.querySelector('.crash');
+const audioCrash = document.querySelector('.a-crash');
+
+const hat = document.querySelector('.hat');
+const audioHat = document.querySelector('.a-hat');
+
+const snare = document.querySelector('.snare');
+const audioSnare = document.querySelector('.a-snare');
+
+const tom = document.querySelector('.tom');
+const audioTom = document.querySelector('.a-tom');
+
+const settingKick = document.querySelectorAll('.setting__kick');
